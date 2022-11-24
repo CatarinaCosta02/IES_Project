20af62b47c33e60e7a822b9727df9ef61cc3f55b
zsikorska <sikorska.zuzia@gmail.com>
2022-11-24 13:15:56 +0000

    Add basic MQ configuration and consumer

---
+++ backend/app/src/main/java/com/whatsnew/app/MQ/Consumer.java
@@ -0,0 +1,11 @@
+package com.whatsnew.app.MQ;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+public class Consumer {
+
+    @RabbitListener(queues = {Config.QUEUE_HN, Config.QUEUE_REDDIT})
+    public void consume(String message){
+        System.out.print(message);
+    }
+}
