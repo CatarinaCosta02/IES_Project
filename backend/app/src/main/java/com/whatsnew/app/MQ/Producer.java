9f39a635d4af7a735f9a3ae16e219d37d4908d49
zsikorska <sikorska.zuzia@gmail.com>
2022-11-29 14:44:55 +0000

    Sending requests for news

---
+++ backend/app/src/main/java/com/whatsnew/app/MQ/Producer.java
@@ -0,0 +1,21 @@
+package com.whatsnew.app.MQ;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableScheduling
+public class Producer {
+
+    @Autowired
+    RabbitTemplate rabbitMQ;
+
+    @Scheduled(fixedRate=1000)
+    public void requestHN(){
+        rabbitMQ.convertAndSend("data_gen", "requests",
+                "{\"type\": \"hn\",\"method\": \"top_stories\"}");
+    }
+}
