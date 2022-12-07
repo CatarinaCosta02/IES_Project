6831c16c9f18d98f7bbda3471cbc6013680e611a
luiscarlos-104171 <luiscarlos.c.afonso@ua.pt>
2022-12-07 15:54:42 +0000

    Added a basic API (Co-Authored By Joao Matos)

--- API/api/src/main/java/com/whatsnew/api/api/Routes.java
+++ API/api/src/main/java/com/whatsnew/api/api/Routes.java
@@ -1,6 +1,10 @@
 package com.whatsnew.api.api;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.whatsnew.api.MQ.Config;
+import com.whatsnew.api.error.RuntimeError;
+import jakarta.websocket.server.PathParam;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,12 +16,15 @@ public class Routes {
     @Autowired
     RabbitTemplate rabbit;
 
-    @Autowired
-    Config config;
+    @GetMapping("/api/search")
+    public String HelloRabbit(@PathParam("query") String query) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        if (query == null) {
+            return mapper.writeValueAsString(new RuntimeError("No query provided"));
+        }
 
-    @GetMapping("/hello")
-    public String HelloRabbit() {
-        rabbit.convertAndSend("api", "api", "Hello");
-        return "Hello";
+        SearchQuery sq = new SearchQuery(query);
+        return (String) rabbit.convertSendAndReceive(Config.EXCHANGE, Config.QUEUE_API, mapper.writeValueAsString(sq));
     }
+
 }
