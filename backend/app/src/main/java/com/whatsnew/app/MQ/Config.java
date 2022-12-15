86347d471f66068347d60c95d2603017a12b1c6c
zsikorska <sikorska.zuzia@gmail.com>
2022-12-15 10:00:21 +0000

    Added mq producer and cosumer for NYT

--- backend/app/src/main/java/com/whatsnew/app/MQ/Config.java
+++ backend/app/src/main/java/com/whatsnew/app/MQ/Config.java
@@ -7,28 +7,28 @@ import org.springframework.amqp.core.*;
 @Configuration
 public class Config {
 
+    // queues names
     public final static String QUEUE_HN = "hn";
     public final static String QUEUE_REDDIT = "reddit";
     public final static String QUEUE_NYT = "nyt";
-
     public final static String QUEUE_REQUEST = "requests";
-
     public static final String QUEUE_API = "api";
+
+    // exchanges names
     public final static String EXCHANGE = "finished_data";
     public final static String EXCHANGE_REQUEST = "data_gen";
-
     public final static String EXCHANGE_API = "api";
+
+    // routing keys names
     public final static String ROUTING_KEY_HN = "hn";
     public final static String ROUTING_KEY_REDDIT = "reddit";
     public final static String ROUTING_KEY_NYT = "nyt";
-
     public final static String ROUTING_KEY_REQUEST = "requests";
-
     public final static String ROUTING_KEY_API = "api";
 
 
 
-    // spring bean for rabbitmq queue
+    // spring beans for rabbitmq queue
     @Bean
     public Queue queueHN() {
         return new Queue(QUEUE_HN, true);
@@ -54,7 +54,7 @@ public class Config {
         return new Queue(QUEUE_API, true);
     }
 
-    // spring bean for rabbitmq exchange
+    // spring beans for rabbitmq exchange
     @Bean
     public DirectExchange exchange() {
         return new DirectExchange(EXCHANGE, false, false);
