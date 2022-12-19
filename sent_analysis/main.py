d452d88f7bca834fa25327bd79dafdf9a33f345e
luiscarlos-104171 <luiscarlos.c.afonso@ua.pt>
2022-12-19 17:54:44 +0000

    Improvements in the model leading to higher accuracy

--- sent_analysis/main.py
+++ sent_analysis/main.py
@@ -11,21 +11,24 @@ from tensorflow.keras.layers import Embedding, LSTM, Dense, Bidirectional
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 
-print(tf.config.list_physical_devices('GPU'))
+train = pd.read_csv('dataset/train.csv', encoding='latin-1', header=None)
+test = pd.read_csv('dataset/test.csv', encoding='latin-1', header=None)
 
-train = pd.read_csv('dataset/train.csv', encoding='latin-1')
-test = pd.read_csv('dataset/test.csv', encoding='latin-1')
+train = train.sample(frac=1).reset_index(drop=True)
+test = test.sample(frac=1).reset_index(drop=True)
 
-train = train.sample(frac=0.1).reset_index(drop=True)
-test = test.sample(frac=0.1).reset_index(drop=True)
+n_records_per_class = 10000
 
-train_X = train[train.columns[5]]
+# train_X = train[train.columns[5]]
+train_X = pd.concat([train[train[0] == 0].head(n_records_per_class), train[train[0] == 4].head(n_records_per_class)])
+train_X = train_X[train_X.columns[5]]
 train_Y = train[train.columns[0]] / 4
+train_Y = pd.concat([train_Y[train[0] == 0].head(n_records_per_class), train_Y[train[0] == 4].head(n_records_per_class)])
 
-test_X = test[test.columns[5]]
+test_X = pd.concat([test[test[0] == 0].head(n_records_per_class), test[test[0] == 4].head(n_records_per_class)])
+test_X = test_X[test.columns[5]]
 test_Y = test[test.columns[0]] / 4
-
-# print(train_X, train_Y, test_X, test_Y)
+test_Y = pd.concat([test_Y[test[0] == 0].head(n_records_per_class), test_Y[test[0] == 4].head(n_records_per_class)])
 
 tokenizer = Tokenizer(num_words=5000)
 tokenizer.fit_on_texts(train_X)
