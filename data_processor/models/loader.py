54b38157120ac4e46d3bb7a42ddebd573f771b3c
luiscarlos-104171 <luiscarlos.c.afonso@ua.pt>
2022-12-20 09:55:30 +0000

    Changed the model used in the inferencing

--- data_processor/models/loader.py
+++ data_processor/models/loader.py
@@ -1,7 +1,7 @@
 import pickle
 import tensorflow as tf
 
-MODEL = "60"  # percentage of accuracy on validation set
+MODEL = "75"  # percentage of accuracy on validation set
 
 tokenizer = pickle.load(open(f"models/{MODEL}.pickle", "rb"))
 model = tf.keras.models.load_model(f"models/{MODEL}.h5")
